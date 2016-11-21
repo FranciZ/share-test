2d0bb0517d259864da93a464b819b89b820eee5a
FranciZ <franci@proxima.si>
2016-11-21 17:52:13 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+angular.module('amsTest', ['ui.bootstrap','ui.router','ngAnimate','djds4rce.angular-socialshare']);
+
+angular.module('amsTest').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('amsTest').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
+
+angular.module('amsTest').run(function($FB){
+    $FB.init('331360950579708');
+});
